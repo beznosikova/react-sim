1814b4826bdbee761ad5ee116715dbacd8ba750a
beznosikova <tanya.beznosikova@avivi.com.ua>
2018-07-02 13:02:10 +0300

    add store stage to localStorage

--- src/index.js
+++ src/index.js
@@ -4,20 +4,29 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import throttle from 'lodash/throttle'
 
 import './css/reset.css';
 import './css/style.css';
 import './css/media.css';
 
+import { loadState, saveState } from './localStorage'
 import reducers from "./reducers";
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const localStorageState = loadState();
+
+
 const store = createStore(
   reducers,
+  localStorageState,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+store.subscribe(throttle(() => {
+    saveState(store.getState())
+}, 1000))
 
 ReactDOM.render(
 	<Provider store={store}>
